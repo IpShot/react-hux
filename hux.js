08902424882b18aa29a0cea8c6d50168eeca67da
IpShot <zhernosek.roman@gmail.com>
2020-06-30 15:16:39 +0500

    add use cache option

--- hux.js
+++ hux.js
@@ -56,8 +56,8 @@ function runSubscriptions(subsObj) {
   subsKeys.forEach(key => subsObj[key]());
 }
 
-function createStore(storeName, reducer, initialState) {
-  const [state, dispatch] = useReducer(reducer, dock[storeName]?.state || initialState);
+function createStore(storeName, reducer, initialState, cache) {
+  const [state, dispatch] = useReducer(reducer, cache && dock[storeName]?.state || initialState);
   if (dock[storeName]) {
     dock[storeName].state = state;
   }
